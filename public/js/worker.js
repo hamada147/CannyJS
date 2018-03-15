0f548a2f39d429732191ad4d5924d214f4ef07f1
Ahmed Moussa <moussa.ahmed95@gmail.com>
2018-03-15 14:51:49 +0200

    Create worker.js

---
+++ public/js/worker.js
@@ -0,0 +1,10 @@
+onmessage = function(e) {
+  importScripts("CannyJS.js");
+  console.log('Message received from main script');
+  let CannyJS = new CannyEdgeDetection();
+  // canvasWidth, canvasHeight, canvasRawDate, ht, lt, sigmma, kernelSize
+  let image = CannyJS.cannyWorker(e.data[0], e.data[1], e.data[2], e.data[3], e.data[4], e.data[5], e.data[6]);
+  let workerResult = image;
+  console.log('Posting message back to main script');
+  postMessage(workerResult);
+}
